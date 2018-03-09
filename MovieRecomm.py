27c7039662d290e815e817b02ac89c1099d6dce3
congyingTech <congyingTech@163.com>
2018-03-09 17:04:21 +0800

    目录结构

--- MovieRecomm.py
+++ MovieRecomm.py
@@ -1,12 +1,9 @@
-from flask import Flask
-
-app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World!'
+#
+#
+# if __name__ == '__main__':
+#     app.run()
---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from config import config
+from flask_login import LoginManager
+
+bootstrap = Bootstrap()
+mail = Mail()
+moment = Moment()
+db = SQLAlchemy()
+
+def createApp(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+    return app
