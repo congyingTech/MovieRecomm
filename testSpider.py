75e96bdbc613106e17bc84287d8382c723400d98
congyingTech <congyingTech@163.com>
2018-03-15 17:44:41 +0800

    用户登录注册已经完成，爬虫开发中

--- testSpider.py
+++ testSpider.py
@@ -3,21 +3,42 @@ from lxml import etree
 
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'}
 
+
+'''
+抓取一个页面的所有电影的url
+'''
+def main(url):
+    response = requests.get(url, headers=headers)
+    html = response.text
+    tree = etree.HTML(html)
+    movie_urls = tree.xpath('//div[@id="content"]/div/div/a[@href]/@href')
+    print(movie_urls)
+    for url in movie_urls:
+        spider(url)
+
+
 def spider(url):
     response = requests.get(url, headers=headers)
     html = response.text
     tree = etree.HTML(html)
-    movie_xpath = tree.xpath('//div[@id="content"]/div/div/a[@href]/@href')
-    title = tree.xpath('//div[@id="content"]/div/h2/a/@title')
+
+    title = tree.xpath('//div[@id="content"]/div/h2/text()');new_title = []
+    for i in title:
+        new_title.append(i.strip().split('》')[0][1:])
+    title = new_title
     content = tree.xpath('//div[@id="content"]/div/div/p/text()')
-    createTime = tree.xpath('//div[@id="content"]/div/div/span[@class="post-info-date"]/a/text()')
-    print(title)
-    print(movie_xpath)
+    for item in enumerate(content):
+        if len(item) == 0:
+            item = tree.xpath('//div[@id="content"]/div/div/div/p/text()')
     print(content)
-    print(createTime)
+    createTime = tree.xpath('//div[@id="content"]/div/div/span[@class="post-info-date"]/a/text()')
+
 
 
 
 
 if __name__ == "__main__":
-    spider('http://www.kaixindy.com/category/video')
+    # maxPage = 355
+    # for i in range(maxPage):
+    #     main('http://www.kaixindy.com/category/video/page/%d'%i)
+    main('http://www.kaixindy.com/category/video/')
